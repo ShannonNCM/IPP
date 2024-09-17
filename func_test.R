65ce4eec627582e80a3cea59649c71ff446db270
ShannonNCM <61364989+ShannonNCM@users.noreply.github.com>
2024-09-17 14:36:10 -0600

    new update

--- func_test.R
+++ func_test.R
@@ -1,6 +1,7 @@
 #_______________________________________________________________________________
 
 #SECCION DE FUNCIONES PARA EL CALCULO DEL INDICE
+#_______________________________________________________________________________
 
 #funcion para obtener las variables de mes actual, mes anterior y meses del año
 inputdata <- function(month_number) {
@@ -41,12 +42,29 @@ calc_indx <- function(col_name){
     mutate(indice = sum1/sum2)
 }
 
+
+
 #_______________________________________________________________________________
 
 #SECCION DE FUNCIONES PARA IMPUTACION
+#_______________________________________________________________________________
 
 #esta seria una lista de productos que si se podrian imputar porque tienen 3 o mas variedades
-can_imput <- c('C101041', 'C103011', 'C103012', 'C103013', 'C103014', 'C104011', 'C105012', 'C106111', 'C106131', 'C106211', 'C107112', 'C107311', 'C107511', 'C107911', 'C108011', 'C110111', 'C110211', 'C110411', 'C131211', 'C139211', 'C141011', 'C151211', 'C152011', 'C161011', 'C162111', 'C170911', 'C170211', 'C201121', 'C201211', 'C202111', 'C202211', 'C202311', 'C202911', 'C210011', 'C222011', 'C231011', 'C239211', 'C239212', 'C239511', 'C239611', 'C239911', 'C241011', 'C259911', 'C281911', 'C310011', 'C322111', 'C323011', 'C329011')
+can_imput <- c('C101041', 'C103011', 'C103012', 'C103013', 'C103014', 'C104011', 
+               'C105012', 'C106111', 'C106131', 'C106211', 'C107112', 'C107311', 
+               'C107511', 'C107911', 'C108011', 'C110111', 'C110211', 'C110411', 
+               'C131211', 'C139211', 'C141011', 'C151211', 'C152011', 'C161011', 
+               'C162111', 'C170911', 'C170211', 'C201121', 'C201211', 'C202111', 
+               'C202211', 'C202311', 'C202911', 'C210011', 'C222011', 'C231011', 
+               'C239211', 'C239212', 'C239511', 'C239611', 'C239911', 'C241011', 
+               'C259911', 'C281911', 'C310011', 'C322111', 'C323011', 'C329011',
+               #agrengando productos que tienen 2 variedades
+               'C101011', 'C101021', 'C101031', 'C102011', 'C102012', 'C104012',
+               'C105011', 'C107111', 'C107121', 'C107211', 'C107411', 'C110412',
+               'C120011', 'C131111', 'C161012', 'C162211', 'C162311', 'C162911',
+               'C170212', 'C181111', 'C221111', 'C221911', 'C222012', 'C242011',
+               'C251111', 'C251211', 'C282511', 'C321111'
+               )
 
 
 #funcion para realizar la imputacion con los precios anteriores
@@ -64,44 +82,67 @@ imputacion <- function(){
     filter(!(PRODUCTO %in% can_imput))
   
   # Se calcula el relativo los productos del mes actual
-  precios_imp <- precios %>% 
-    select(PRODUCTO, VARIEDAD, all_of(mes), all_of(mesant)) %>% 
-    filter(!!sym(mes) != 0 & !!sym(mesant) != 0) %>% 
-    filter(PRODUCTO %in% can_imput) %>% 
-    mutate(rel_var = !!sym(mes) / !!sym(mesant)) %>% 
-    group_by(PRODUCTO) %>% 
-    summarize(rel_prod = geometric.mean(rel_var))
+  # precios_imp <- precios %>% 
+  #   select(PRODUCTO, VARIEDAD, all_of(mes), all_of(mesant)) %>% 
+  #   filter(!!sym(mes) != 0 & !!sym(mesant) != 0) %>% 
+  #   filter(PRODUCTO %in% can_imput) %>% 
+  #   mutate(rel_var = !!sym(mes) / !!sym(mesant)) %>% 
+  #   group_by(PRODUCTO) %>% 
+  #   summarize(rel_prod = geometric.mean(rel_var))
   
   # Se genera un dataframe con los precios del mes anterior, ya que en algunos casos
   # el mes anterior no presenta precios se toman los promedios de las variedades que
   # pertenecen a un mismo producto
-  precioant_imp <- precios %>% select(PRODUCTO, VARIEDAD, all_of(mes), all_of(mesant)) %>% 
-    #filter(!!sym(mes) != 0 & !!sym(mesant) != 0) %>% 
-    filter(PRODUCTO %in% can_imput) %>% 
-    filter(!!sym(mesant) != 0) %>% 
-    group_by(PRODUCTO) %>% 
-    summarize(media = mean(!!sym(mesant)))
+  # precioant_imp <- precios %>% select(PRODUCTO, VARIEDAD, all_of(mes), all_of(mesant)) %>% 
+  #   #filter(!!sym(mes) != 0 & !!sym(mesant) != 0) %>% 
+  #   filter(PRODUCTO %in% can_imput) %>% 
+  #   filter(!!sym(mesant) != 0) %>% 
+  #   group_by(PRODUCTO) %>% 
+  #   summarize(media = mean(!!sym(mesant)))
+  
+  #se calcula el factor de imputacion utilizando los precios que si se encuentran en el mes actual
+  factor_imput <- bol %>% mutate(variacion = !!sym(mes) / !!sym(mesant)) %>% 
+    mutate(variacion = if_else(is.infinite(variacion), 1, variacion)) %>% 
+    group_by(VARIEDAD) %>% 
+    summarize(factorimput = geometric.mean(variacion))
   
   # Se genera un dataframe con los precios imputados
-  precios_imputados <- precios_imp %>% left_join(precioant_imp, by='PRODUCTO') %>% 
-    mutate(p_imp = rel_prod*media)
-  
-  imputados <- sipuede_imp %>% left_join(precios_imputados, by = 'PRODUCTO') %>% 
-    mutate(!!sym(mes) := p_imp) %>% 
+  # precios_imputados <- precios_imp %>% left_join(precioant_imp, by='PRODUCTO') %>% 
+  #   mutate(p_imp = rel_prod*media)
+  precios_imput <- sipuede_imp %>% left_join(factor_imput, join_by(VARIEDAD)) %>% 
+    mutate(factorimput = if_else(is.na(factorimput), 1, factorimput)) %>% 
+    mutate(!!sym(mes) := !!sym(mesant)*factorimput) %>% 
     select(PRODUCTO, VARIEDAD, !!sym(mes), !!sym(mesant))
   
   # Se genera un dataframe con todos los precios para la realización del cálculo
   boleta <- bol %>% 
-    rbind(imputados) %>% 
+    rbind(precios_imput) %>% 
     rbind(nopuede_imp)
   
   return(boleta)
 }
 
 
+#SECCION DE FUNCIONES PARA EL CALCULO DE INCIDENCIAS Y VARIACIONES
+#_______________________________________________________________________________
+
+#funcion para calcular las incidencias
+incidencias <- function(x){
+  x %>% left_join(ponderaciones, join_by(PRODUCTO)) %>% 
+    rename(indact = indice) %>% 
+    left_join(indant, join_by(PRODUCTO)) %>% 
+    rename(indant = indice) %>% 
+    select(PRODUCTO, indact, ponderacion, indant) %>% 
+    mutate(incid = ((indact-indant)/indgen_ant$indice)*ponderacion)
+}
+
+
+
+
 #_______________________________________________________________________________
 
 #SECCION DE FUNCIONES PARA EXPORTAR DATOS A EXCEL
+#_______________________________________________________________________________
 
 #funcion para obtener los datos que no se tienen datos en el mes actual
 missing_prices <- function(){
@@ -121,7 +162,6 @@ missing_prices <- function(){
 }
 
 
-
 #funcion para exportar los datos de los indices a excel
 export_index <- function(){
   wb <- createWorkbook()
@@ -136,7 +176,6 @@ export_index <- function(){
   writeData(wb, "Sheet4", ind_grupo)
   addWorksheet(wb, "Sheet5")
   writeData(wb, "Sheet5", ind_div)
-  
   addWorksheet(wb, mes)
   writeData(wb, mes, ind)
   
@@ -195,10 +234,10 @@ count_imput <- function(){
 }
 
 
-
 #_______________________________________________________________________________
 
 #SECCION DE FUNCIONES PARA GRAFICAR DATOS
+#_______________________________________________________________________________
 
 #funcion para realizar el grafico del indice
 grafica <- function(data, x_col, y_col) {
@@ -208,52 +247,3 @@ grafica <- function(data, x_col, y_col) {
     labs(x = x_col, y = y_col) +
     theme_gray()
 }
-
-
-
-
-
-
-#funcion para realizar la imputacion (este procedimiento es provisional con los
-# datos que se tienen hasta ahora)
-# impute_prices <- function(mes, mesant) {
-#   
-#   # Estos serían los que sí se pueden imputar
-
-#   sipuede_imp <- precios %>% 
-#     select(PRODUCTO, VARIEDAD, all_of(mes), all_of(mesant)) %>% 
-#     filter(!!sym(mes) == 0) %>% 
-#     filter(PRODUCTO %in% can_imput)
-#   
-#   # Estos serían los que no se pueden imputar
-#   nopuede_imp <- precios %>% 
-#     select(PRODUCTO, VARIEDAD, all_of(mes), all_of(mesant)) %>% 
-#     filter(!!sym(mes) == 0) %>% 
-#     filter(!(PRODUCTO %in% can_imput))
-#   
-#   # Estos serían los que no son cero sobre los que se va a calcular el relativo para imputación
-#   precios_imp <- precios %>% 
-#     select(PRODUCTO, VARIEDAD, all_of(mes), all_of(mesant)) %>% 
-#     filter(!!sym(mes) != 0 & !!sym(mesant) != 0) %>% 
-#     filter(PRODUCTO %in% can_imput) %>% 
-#     mutate(rel_var = !!sym(mes) / !!sym(mesant)) %>% 
-#     group_by(PRODUCTO) %>% 
-#     summarize(rel_prod = geometric.mean(rel_var),
-#               #media = median(!!sym(mes)) #esto es usando la mediana
-#               media = mean(!!sym(mes)) #esto es usando la media
-#               ) %>% 
-#     mutate(p_imp = rel_prod * media)
-#   
-#   # Se obtiene el dataframe con los precios imputados
-#   imputados <- sipuede_imp %>% 
-#     left_join(precios_imp, by = "PRODUCTO") %>% 
-#     mutate(!!sym(mes) := p_imp) %>% 
-#     select(PRODUCTO, VARIEDAD, !!sym(mes), !!sym(mesant))
-#   
-#   # Se regresan los precios imputados y los que no se pudieron imputar a la boleta
-#   boleta <- bol %>% 
-#     rbind(imputados) %>% 
-#     rbind(nopuede_imp)
-#   
-#   return(boleta)
-# }
